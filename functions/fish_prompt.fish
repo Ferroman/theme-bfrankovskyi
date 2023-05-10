54255eee9cd7b80a1676b196583c16caefd9f91f
Bohdan F <bohdan.f@affinidi.com>
2023-05-10 14:36:19 +0300

    feat: show number of ahead and behind commits

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,7 +3,7 @@
 # Function to get the name of the current git branch
 # Returns empty string if not in a git repository
 function __git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+  echo (command git branch --show-current 2> /dev/null)
 end
 
 # Function to check if there are uncommitted changes in the current git repository
@@ -21,9 +21,17 @@ end
 # Function to get the number of commits ahead of the remote tracking branch
 # of the current git branch
 # Returns 0 if there are no commits ahead
-function __git_pending_commits_count -a branch_name
-  echo (command git branch -v -l "$branch_name" 2> /dev/null | \
-    grep 'ahead' | wc -l | tr -d ' ')
+function __git_ahead_commits_count 
+  set -l branch_name (__git_branch_name)
+  echo (command git rev-list origin/$branch_name..HEAD --count 2> /dev/null)
+end
+
+# Function to get the number of commits behind of the remote tracking branch
+# of the current git branch
+# Returns 0 if there are no commits behind
+function __git_behind_commits_count 
+  set -l branch_name (__git_branch_name)
+  echo (command git rev-list HEAD..origin/$branch_name --count 2> /dev/null)
 end
 
 # Function to print current time with the specified color
@@ -78,18 +86,23 @@ end
 # with the specified colors for the git branch, normal text,
 # brackets, unpushed commits, and dirty repository status
 # If not in a git repository, returns an empty string
-function _print_git_status -a main_color -a color_normal -a color_bracket -a color_unpushed -a color_dirty
+function _print_git_status -a main_color -a color_normal -a color_bracket -a color_unpushed -a color_dirty -a color_unpulled
   set -l git_branch_name (__git_branch_name)
 
   if [ $git_branch_name ]
-    set -l git_pending_commits_count (__git_pending_commits_count $git_branch_name)
+    set -l git_pending_commits_count (__git_ahead_commits_count)
+    set -l git_fetch_commits_count (__git_behind_commits_count)
 
     set -l git_branch (_print_branch_name $git_branch_name $main_color $color_unpushed $color_normal)
 
     set -l git_status "$color_bracket ($git_branch$color_bracket)$color_normal"
 
     if [ $git_pending_commits_count != 0 ]
-      set git_status "$git_status $color_unpushed+$git_pending_commits_count$color_normal"
+      set git_status "$git_status $color_unpushed↑$git_pending_commits_count$color_normal"
+    end
+
+    if [ $git_fetch_commits_count != 0 ]
+      set git_status "$git_status $color_unpulled↓$git_fetch_commits_count$color_normal"
     end
 
     if [ (__is_git_dirty) ]
@@ -116,6 +129,7 @@ function fish_prompt
   set -l color_cwd (set_color -o $fish_color_cwd)
   set -l color_git_info (set_color -o $fish_color_command)
   set -l color_unpushed (set_color -o $fish_color_user)
+  set -l color_unpulled (set_color -o $fish_color_error)
   set -l color_git_dirty (set_color -o $fish_color_param)
   set -l color_bracket (set_color -o $normal)
   set -l color_git_branch (set_color -o $fish_color_operator)
@@ -125,7 +139,7 @@ function fish_prompt
   set -l time (_print_time $color_normal $color_normal)
 
   set -l cwd $color_cwd(basename (prompt_pwd))$color_normal
-  set -l git_status (_print_git_status $color_git_branch $color_normal $color_bracket $color_unpushed $color_git_dirty)
+  set -l git_status (_print_git_status $color_git_branch $color_normal $color_bracket $color_unpushed $color_git_dirty $color_unpulled)
 
   echo -n -s $arrow $time ' ' $cwd $git_status ' ' $failed $color_normal ''
 end
